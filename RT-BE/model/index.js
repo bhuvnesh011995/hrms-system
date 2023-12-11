4049e08a4cc41ccad06ec9dfab517f4867adb787
Ritik <ritik@braincavesoft.com>
2023-12-11 11:56:26 +0530

    project setup

--- RT-BE/model/index.js
+++ RT-BE/model/index.js
@@ -1,26 +1,23 @@
 module.exports = {
-    employee:require("./staff/employee.model"),
-    role:require("./role.model"),
-    constant:require("./system/constant.model"),
-    company:require("./company.model"),
-    department:require("./department.model"),
-    subdepartment:require("./subdepartment.model"),
-    location:require("./location.model"),
-    policy:require("./policy.model"),
-    designation:require("./designation.model"),
-    announcement:require("./announcements.model"),
-    documents:require("./documents.model"),
-    officeShift:require("./timesheet/officeShift.model"),
-    award:require("./coreHR/award.model"),
-    transfer:require("./coreHR/transfer.model"),
-    paymentGateway:require("./system/paymentGateway.model"),
-    system:require("./system/setting/system.model"),
-    constants:require("./system/constatntModels"),
-    modules:require("./system/modules/modules.model"),
+  employee: require("./staff/employee.model"),
+  role: require("./role.model"),
+  constant: require("./system/constant.model"),
+  company: require("./company.model"),
+  department: require("./department.model"),
+  subdepartment: require("./subdepartment.model"),
+  location: require("./location.model"),
+  policy: require("./policy.model"),
+  designation: require("./designation.model"),
+  announcement: require("./announcements.model"),
+  documents: require("./documents.model"),
+  officeShift: require("./timesheet/officeShift.model"),
+  award: require("./coreHR/award.model"),
+  transfer: require("./coreHR/transfer.model"),
+  paymentGateway: require("./system/paymentGateway.model"),
+  system: require("./system/setting/system.model"),
+  constants: require("./system/constatntModels"),
+  modules: require("./system/modules/modules.model"),
 
-    themes:require('./system/themeSetting.model'),
-    language:require('./system/language.model')
-
-    themes:require('./system/themeSetting.model')
-
-}
+  themes: require("./system/themeSetting.model"),
+  language: require("./system/language.model"),
+};
